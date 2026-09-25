b945e781041fb6727749b000e25fb84d9bc655d9
SHIVAM2001-NITP <76029255+SHIVAM2001-NITP@users.noreply.github.com>
2026-09-25 21:47:05 +0530

    Add DFS solution for Keys and Rooms problem
    Implemented DFS algorithm to check if all rooms can be visited.

--- Graph/Keys and Rooms.cpp
+++ Graph/Keys and Rooms.cpp
@@ -31,3 +31,32 @@ public:
         
     }
 };
+//JAVA
+
+class Solution {
+    void dfs(List<List<Integer>> rooms,int u,boolean[] visited) {
+        visited[u]=true;
+
+        for(int node:rooms.get(u)) {
+            if(!visited[node])
+                dfs(rooms,node,visited);
+        }
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n=rooms.size();
+        boolean[] visited=new boolean[n];
+
+        dfs(rooms,0,visited);
+
+        for(boolean x:visited) {
+            if(!x)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+//Time: O(V + E)
+//Space: O(V) for visited + DFS recursion stack.
